83c0b8360adc8701f04eea10e89b8774a34424b5
Max Bazarov <bazaroffm@gmail.com>
2023-05-04 13:02:10 +0300

    replace room model

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,26 +1,14 @@
 from sqlalchemy import JSON, Column, ForeignKey, Integer, String
+
 from app.database import Base
 
 
-class Hotels(Base):
-    __tablename__ = 'hotels'
+class Hotel(Base):
+    __tablename__ = 'hotel'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
     location = Column(String, nullable=False)
     services = Column(JSON)
     rooms_quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
-
-
-class Rooms(Base):
-    __tablename__ = 'rooms'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    hotel_id = Column(ForeignKey('hotels.id'), nullable=False)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    price = Column(Integer, nullable=False)
-    services = Column(JSON)
-    quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
