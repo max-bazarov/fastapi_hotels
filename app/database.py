d24da1af97a178bf75166705c93a329b2def657a
Max Bazarov <bazaroffm@gmail.com>
2023-05-04 12:59:57 +0300

    rename async_session_maker

--- app/database.py
+++ app/database.py
@@ -3,10 +3,9 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-
 engine = create_async_engine(settings.database_url)
 
-async_session_make = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
