8dcca1daa40c882fa67b34a2363f72c0182c7fa9
Max Bazarov <bazaroffm@gmail.com>
2023-05-04 13:05:18 +0300

    add bookings schemas

---
+++ app/bookings/schemas.py
@@ -0,0 +1,30 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class BookingReadSchema(BaseModel):
+    id: int
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
+    total_cost: int
+    total_days: int
+
+    class Config:
+        orm_mode = True
+
+
+class BookingCreateSchema(BaseModel):
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
+    total_cost: int
+    total_days: int
+
+    class Config:
+        orm_mode = True
